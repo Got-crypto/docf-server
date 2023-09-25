adb1010a54a16b69dbf3f3b727e3bdb35af49e8d
got-crypto <pecpalmplay@gmail.com>
2023-09-26 01:38:25 +0200

    connected to mongodb

---
+++ index.js
@@ -0,0 +1,18 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from "dotenv"
+import express from 'express'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+    .then(() => app.listen(PORT, () => console.log("listening to port ", PORT)))
+    .catch((error) => console.log('error', error))
